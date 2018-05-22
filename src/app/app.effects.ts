226b900c28f6320585787bdea177ebfd57dbf76a
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-05-22 19:11:52 +0200

    Implement Location fetching effects

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { defer, Observable, of } from 'rxjs';
+import { LoadLocations, LocationActionTypes, FetchLocationsError } from './state/location/location.actions';
+import { LocationService } from './services/location.service';
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { ErrorHandlingService } from './services/error-handling.service';
 
 
 @Injectable()
 export class AppEffects {
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private location: LocationService,
+    private errorHandling: ErrorHandlingService,
+  ) {}
+
+  @Effect({dispatch: false})
+  ajaxError$ = this.actions$.pipe(
+    ofType(
+      LocationActionTypes.FetchLocationsError,
+    ),
+    map((action: FetchLocationsError) => action.payload.error),
+    map(error => this.errorHandling.handle(error)),
+  );
+
+  @Effect()
+  loadLocations$ = this.actions$.pipe(
+    ofType(LocationActionTypes.FetchLocations),
+    switchMap(() => this.location.getLocations()),
+    map(locations => new LoadLocations({locations})),
+    catchError(error => of(new FetchLocationsError(error)))
+  );
 }
