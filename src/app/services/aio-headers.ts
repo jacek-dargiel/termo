b0aed141e650252b13d43888f77e0d0cc6a80e6a
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-05-30 23:43:45 +0200

    Missing token warning

--- src/app/services/aio-headers.ts
+++ src/app/services/aio-headers.ts
@@ -2,6 +2,10 @@ import { HttpHeaders } from '@angular/common/http';
 
 let token = window.localStorage.getItem('AIO_TOKEN');
 
+if (!token) {
+    console.warn('Development token not found in local storage!');
+}
+
 export let aioHeaders;
 
 if (token) {
