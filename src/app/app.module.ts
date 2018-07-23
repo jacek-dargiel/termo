59223d3ee9193de979102b29908a52c668852d08
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-07-23 22:19:53 +0200

    Fix some import paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './state/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import { environment } from 'environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
 import * as fromLocation from './state/location/location.reducer';
