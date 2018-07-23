67d5d9157be627cf5d83077ad454ef35bf0a7244
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-07-23 21:03:48 +0200

    fix lodash import paths

--- src/app/helpers/utils.ts
+++ src/app/helpers/utils.ts
@@ -1,6 +1,6 @@
-import { flow } from 'lodash/fp/flow';
-import { fromPairs } from 'lodash/fp/fromPairs';
-import { map } from 'lodash/fp/map';
+import flow from 'lodash/fp/flow';
+import fromPairs from 'lodash/fp/fromPairs';
+import map from 'lodash/fp/map';
 import { Dictionary } from '@ngrx/entity/src/models';
 
 export function mapToObject<T>(iteratee: ((v: any) => T), arr: Array<any>): Dictionary<T> {
