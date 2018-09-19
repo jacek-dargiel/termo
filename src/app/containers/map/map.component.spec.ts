4b8bff9fd9e64b53b2e68beee1fa4d12b1e5c279
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-09-19 23:35:17 +0200

    Mock MapFacade for map component tests

--- src/app/containers/map/map.component.spec.ts
+++ src/app/containers/map/map.component.spec.ts
@@ -2,6 +2,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { MapComponent } from './map.component';
+import { MapLocationComponent } from '../../components/map-location/map-location.component';
+import { MapFacade } from './map.facade';
+import { Observable, of } from 'rxjs';
+import { LocationWithKeyMeasurmentValues, Location } from '../../state/location/location.model';
+import { ToFixedPipe } from '../../to-fixed.pipe';
+import { IsLocationOutdatedPipe } from '../../pipes/is-location-outdated.pipe';
+import { RelativeTimePipe } from '../../pipes/relative-time.pipe';
+
+class MockMapFacade {
+  public locationsLoading$: Observable<boolean>;
+  public locations$: Observable<LocationWithKeyMeasurmentValues>;
+  public selectedLocation$: Observable<Location>;
+
+  public dispatchMapInit() {}
+  public getImageDimentions() {
+    return of({width: 800, height: 600});
+  }
+  public selectLocation(location: Location) {}
+}
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -9,7 +28,16 @@ describe('MapComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ],
+      declarations: [
+        MapComponent,
+        MapLocationComponent,
+        ToFixedPipe,
+        IsLocationOutdatedPipe,
+        RelativeTimePipe,
+      ],
+      providers: [
+        { provide: MapFacade, useClass: MockMapFacade },
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
