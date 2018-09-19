67d0e8702cffa68931dc0fda873244ee3683127e
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-09-19 22:29:24 +0200

    Pass test location to map location component

--- src/app/components/map-location/map-location.component.spec.ts
+++ src/app/components/map-location/map-location.component.spec.ts
@@ -5,6 +5,7 @@ import { MapLocationComponent } from './map-location.component';
 import { ToFixedPipe } from '../../to-fixed.pipe';
 import { IsLocationOutdatedPipe } from '../../pipes/is-location-outdated.pipe';
 import { RelativeTimePipe } from '../../pipes/relative-time.pipe';
+import { LocationWithKeyMeasurmentValues } from '../../state/location/location.model';
 
 describe('MapLocationComponent', () => {
   let component: MapLocationComponent;
@@ -26,6 +27,19 @@ describe('MapLocationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MapLocationComponent);
     component = fixture.componentInstance;
+    let testLocation: LocationWithKeyMeasurmentValues = {
+      id: 'temperatura.gorny_tunel',
+      name: 'Górny Tunel',
+      mapPosition: {
+        x: 0.25,
+        y: 0.75,
+      },
+      updatedAt: new Date('2018-09-19T22:10:00'),
+      lastMeasurmentValue: 21.12,
+      minimalMeasurmentValue: 19.5,
+    };
+    component.location = testLocation;
+
     fixture.detectChanges();
   });
 
