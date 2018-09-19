84bd4c5ce0f0ccc68bd6c435aeb3317f9959cdb0
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-09-19 19:47:58 +0200

    Mock dependencies in service tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,11 +1,22 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+class MockClient {
+  get() {
+    return of();
+  }
+}
 
 describe('ApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ApiService]
+      providers: [
+        ApiService,
+        { provide: HttpClient, useClass: MockClient },
+      ]
     });
   });
 
--- src/app/services/measurment.service.spec.ts
+++ src/app/services/measurment.service.spec.ts
@@ -1,15 +1,56 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { of } from 'rxjs';
 
 import { MeasurmentService } from './measurment.service';
+import { ApiService } from './api.service';
+
+class MockApiSerivce {
+  get() {
+    return of([
+      {
+        id: '123',
+        value: '20.1',
+        feed_id: 1234,
+        feed_key: 'temperatura.gorny_tunel',
+        created_at: '2018-09-19T15:45:00Z',
+        created_epoch: 1537371900,
+      },
+      {
+        id: '124',
+        value: '20.2',
+        feed_id: 1234,
+        feed_key: 'temperatura.gorny_tunel',
+        created_at: '2018-09-19T15:48:00Z',
+        created_epoch: 1537372080,
+      }
+    ]);
+  }
+}
 
 describe('MeasurmentService', () => {
+  let api: ApiService;
+  let service: MeasurmentService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MeasurmentService]
+    let bed = TestBed.configureTestingModule({
+      providers: [
+        MeasurmentService,
+        { provide: ApiService, useClass: MockApiSerivce }
+      ]
     });
+    api = bed.get(ApiService);
+    service = bed.get(MeasurmentService);
   });
 
-  it('should be created', inject([MeasurmentService], (service: MeasurmentService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+  it('should get measurments', (done) => {
+    service.getMeasurments('123')
+      .subscribe(measurments => {
+        expect(measurments.length).toBe(2);
+        done();
+      });
+  });
 });
