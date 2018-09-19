7aa018a4e3b11be8dd1337af927244e8e64ddc8b
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-09-19 15:43:38 +0200

    isLocationOutdated pipe tests

--- src/app/pipes/is-location-outdated.pipe.spec.ts
+++ src/app/pipes/is-location-outdated.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { IsLocationOutdatedPipe } from './is-location-outdated.pipe';
 
 describe('IsLocationOutdatedPipe', () => {
+  let pipe = new IsLocationOutdatedPipe();
+  let since = new Date('2018-09-19T15:40:00');
   it('create an instance', () => {
-    const pipe = new IsLocationOutdatedPipe();
     expect(pipe).toBeTruthy();
+  });
+  it('should return `true` if provided date is outdated', () => {
+    let targetDate = new Date('2018-09-19T15:20:00');
+    let result = pipe.transform(targetDate, since);
+    expect(result).toBe(true);
+  });
+  it('should return `false` if provided date is not outdated', () => {
+    let targetDate = new Date('2018-09-19T15:35:00');
+    let result = pipe.transform(targetDate, since);
+    expect(result).toBe(false);
   });
 });
