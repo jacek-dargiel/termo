b7109d5d1ef1c57d59a1693f462e2e53c71c49fb
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-09-29 22:48:04 +0200

    Add some basic chart tests

--- src/app/containers/chart/chart.component.spec.ts
+++ src/app/containers/chart/chart.component.spec.ts
@@ -3,7 +3,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ChartComponent } from './chart.component';
 import { ChartFacade } from './chart.facade';
-import { Observable, of } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 import { Location } from '../../state/location/location.model';
 import { Measurment } from '../../state/measurment/measurment.model';
 
@@ -13,6 +13,15 @@ let mockLocation: Location = {
   mapPosition: {
     x: 0.25,
     y: 0.75,
+  },
+  updatedAt: new Date('2018-09-19T23:45:00'),
+};
+let mockLocation2: Location = {
+  id: 'temperatura.dolny_tunel',
+  name: 'Dolny Tunel',
+  mapPosition: {
+    x: 0.5,
+    y: 0.5,
   },
   updatedAt: new Date('2018-09-19T23:45:00'),
 };
@@ -34,15 +43,23 @@ let mockMeasurmnets: Measurment[] = [
 ];
 
 class MockChartFacade {
-  public selectedLocation$: Observable<Location> = of(mockLocation);
-  public selectedLocationMeasurments$: Observable<Measurment[]> = of(mockMeasurmnets);
+  public selectedLocation$ = new ReplaySubject<Location>();
+  public selectedLocationMeasurments$ = new ReplaySubject<Measurment[]>();
 
-  public closeChart() {}
+  constructor() {
+    this.selectedLocation$.next(mockLocation);
+    this.selectedLocationMeasurments$.next(mockMeasurmnets);
+  }
+
+  public closeChart() {
+    this.selectedLocation$.next(undefined);
+  }
 }
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
   let fixture: ComponentFixture<ChartComponent>;
+  let facade: MockChartFacade;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -53,6 +70,7 @@ describe('ChartComponent', () => {
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
+    facade = TestBed.get(ChartFacade);
   }));
 
   beforeEach(() => {
@@ -64,4 +82,22 @@ describe('ChartComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should render chart', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+  it('should hide chart when closed', () => {
+    component.close();
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+  it('should render new location when location changes', () => {
+    facade.selectedLocation$.next(mockLocation2);
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+  // it('should render new measurments when measurments change', () => {
+  //   facade.selectedLocationMeasurments$.next(mockMeasurmnets2);
+  //   fixture.detectChanges();
+  //   let chart = fixture.debugElement.query(By.css('ngx-charts-line-chart'));
+  // });
 });
