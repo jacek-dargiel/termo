609bb3371ab004b49fc31dc7dd1a5a7c6a9aa494
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-09-29 23:26:00 +0200

    Add header tests

--- src/app/containers/header/header.component.spec.ts
+++ src/app/containers/header/header.component.spec.ts
@@ -2,18 +2,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 import { HeaderFacade } from './header.facade';
-import { Observable, of } from 'rxjs';
+import { Observable, of, ReplaySubject } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 class MockHeaderFacade {
-  public progress: Observable<number> = of(0.5);
-  public refreshing: Observable<boolean>;
+  public progress = new ReplaySubject<number>();
+  public refreshing = new ReplaySubject<boolean>();
 
-  public refresh() {}
+  constructor() {
+    this.progress.next(0);
+    this.refreshing.next(false);
+  }
+
+  public refresh = jest.fn();
 }
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let facade: MockHeaderFacade;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,15 +30,42 @@ describe('HeaderComponent', () => {
       ]
     })
     .compileComponents();
+    facade = TestBed.get(HeaderFacade);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render header at initial state', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should render header at 50%', () => {
+    facade.progress.next(0.5);
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should render header at refreshing state', () => {
+    facade.progress.next(1);
+    facade.refreshing.next(true);
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should refresh when "Refresh" button clicked', () => {
+    let button = fixture.debugElement.query(By.css('button.refresh'));
+    button.nativeElement.click();
+    fixture.detectChanges();
+    expect(facade.refresh).toHaveBeenCalled();
+  });
+
 });
