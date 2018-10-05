d7eb637e3b5838a046ffe80b63ead9314d659719
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-10-05 21:54:01 +0200

    Snackbar component tests

--- src/app/components/snackbar/snackbar.component.spec.ts
+++ src/app/components/snackbar/snackbar.component.spec.ts
@@ -1,16 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SnackbarComponent } from './snackbar.component';
+import { Subject } from 'rxjs';
+import { SnackbarService } from '../../services/snackbar.service';
+
+class MockSnapshotService {
+  messages = new Subject();
+}
 
 describe('SnackbarComponent', () => {
   let component: SnackbarComponent;
   let fixture: ComponentFixture<SnackbarComponent>;
+  let service: MockSnapshotService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SnackbarComponent ]
+      declarations: [ SnackbarComponent ],
+      providers: [
+        { provide: SnackbarService, useClass: MockSnapshotService }
+      ],
     })
-    .compileComponents();
+      .compileComponents();
+
+    service = TestBed.get(SnackbarService);
   }));
 
   beforeEach(() => {
@@ -21,5 +33,18 @@ describe('SnackbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the snackbar component', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should render a snackbar when message recived', () => {
+    let message = {
+      message: 'Something is wrong',
+    };
+    service.messages.next(message);
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
   });
 });
