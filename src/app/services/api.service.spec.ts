b11276f07436fd60c43ac6babae2d92a253bdc97
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-10-17 22:04:31 +0200

    Add ApiService tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
-import { of } from 'rxjs';
+import { empty } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 class MockClient {
-  get() {
-    return of();
-  }
+  get = jest.fn(() => empty());
 }
 
 describe('ApiService', () => {
+  let client;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -18,9 +17,15 @@ describe('ApiService', () => {
         { provide: HttpClient, useClass: MockClient },
       ]
     });
+    client = TestBed.get(HttpClient);
   });
 
   it('should be created', inject([ApiService], (service: ApiService) => {
     expect(service).toBeTruthy();
+  }));
+  it('should pass to the HttpClient', inject([ApiService], (service: ApiService) => {
+    let someSampleOptions = { option: 1 };
+    service.get('/example/url', someSampleOptions );
+    expect(client.get).toHaveBeenCalledWith('/api/example/url', someSampleOptions);
   }));
 });
