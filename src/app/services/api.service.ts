e677684a04c1ef793ff8d4021d10a3a3c613c5c3
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-10-17 20:04:35 +0200

    Remove AIO key support
    Feeds are now public, using keys is not required anymore.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { tap } from 'rxjs/operators';
 import { isEmpty } from '../helpers/lodash';
@@ -8,25 +8,16 @@ import { isEmpty } from '../helpers/lodash';
   providedIn: 'root'
 })
 export class ApiService {
-  token = window.localStorage.getItem('AIO_TOKEN');
-
   constructor(
     private http: HttpClient,
   ) { }
 
   get<T>(url, options = {}) {
-    if (this.token) {
-      let aioHeaders = new HttpHeaders({
-        'X-AIO-Key': this.token,
-      });
-      options['headers'] = aioHeaders;
-    }
     return this.http.get<T>(`${environment.API_URL}${url}`, options)
       .pipe(
         tap(response => {
-          if (isEmpty(response) && !this.token) {
-            // tslint:disable-next-line:max-line-length
-            console.warn('API response is empty. This might be caused by an unauthorized request. Set `AIO_TOKEN` key in local storage to use it for authorization.');
+          if (isEmpty(response)) {
+            console.warn('API response is empty. Is this AIO feed public?');
           }
         })
       );
