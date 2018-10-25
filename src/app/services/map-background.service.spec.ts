4df6d5a4808e8c4184fcf2f1443515ee81a11d9c
Jacek Dargiel <jacek.dargiel@10clouds.com>
2018-10-25 18:13:22 +0200

    Add super-basic MapBackgroundService tests

--- src/app/services/map-background.service.spec.ts
+++ src/app/services/map-background.service.spec.ts
@@ -1,15 +1,31 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MapBackgroundService } from './map-background.service';
+import { isObservable } from 'rxjs';
 
 describe('MapBackgroundService', () => {
+  let oService: MapBackgroundService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [MapBackgroundService]
     });
+    oService = TestBed.get(MapBackgroundService);
   });
 
   it('should be created', inject([MapBackgroundService], (service: MapBackgroundService) => {
     expect(service).toBeTruthy();
   }));
+  describe('getImageDimentions', () => {
+    // tslint:disable-next-line:max-line-length
+    let imageUrl = 'data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7';
+    it('should return a observable', inject([MapBackgroundService], (service: MapBackgroundService) => {
+      let dimentions$ = service.getImageDimentions(imageUrl);
+      expect(isObservable(dimentions$)).toBeTruthy();
+    }));
+
+    // can't test thease
+    // @see https://github.com/jsdom/jsdom/issues/1816
+    xit('should emit error for invalid URL', () => {} );
+    xit('should emit dimentions', done => {});
+  });
 });
