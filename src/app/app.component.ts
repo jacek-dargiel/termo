8037a8fa6cef8cc04e325e1a5ac824741a9ec9a1
Jacek Dargiel <jacek.dargiel@10clouds.com>
2025-09-11 19:47:28 +0200

    Bootstrap app component with standalone API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MapComponent } from './containers/map/map.component';
+import { ChartComponent } from './containers/chart/chart.component';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
 
 @Component({
     selector: 'termo-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
+    imports: [MapComponent, ChartComponent, SnackbarComponent]
 })
 export class AppComponent {
   title = 'termo';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,41 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ErrorHandler, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from 'app/app.module';
+
 import { environment } from 'environments/environment';
+import { ErrorHandlingService } from './app/services/error-handling.service';
+import { LocationService } from './app/services/location.service';
+import { MeasurmentService } from './app/services/measurment.service';
+import { MapFacade } from './app/containers/map/map.facade';
+import { TERMO_CURRENT_TIME_FACTORY } from './app/pipes/current-time.injection-token';
+import { SentryErrorHandler } from './app/services/sentry.error-handler';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+
+import { reducers, metaReducers } from './app/state/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app/app.effects';
+import { LineChartModule } from '@swimlane/ngx-charts';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, StoreModule.forRoot(reducers, { metaReducers }), !environment.production ? StoreDevtoolsModule.instrument({ serialize: { options: { map: true, set: true } }, connectInZone: true }) : [], EffectsModule.forRoot([AppEffects]), StoreModule.forFeature('location', fromLocation.reducer), StoreModule.forFeature('measurment', fromMeasurment.reducer), LineChartModule),
+        ErrorHandlingService,
+        LocationService,
+        MeasurmentService,
+        MapFacade,
+        { provide: TERMO_CURRENT_TIME_FACTORY, useValue: () => new Date() },
+        { provide: ErrorHandler, useClass: SentryErrorHandler },
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.log(err));
