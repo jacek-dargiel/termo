64b469d7b6007c0cde28bcadbccfd9829bba43ac
Jacek Dargiel <jacek.dargiel@10clouds.com>
2025-09-11 20:23:29 +0200

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,14 +2,19 @@ import { TestBed, waitForAsync } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './containers/map/map.component';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
+import { ChartComponent } from './containers/chart/chart.component';
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [
+      imports: [
         AppComponent
       ],
       schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
+    })
+      .overrideComponent(AppComponent, { remove: { imports: [MapComponent, ChartComponent, SnackbarComponent]}})
+      .compileComponents();
   }));
   it('should create the app', waitForAsync(() => {
     let fixture = TestBed.createComponent(AppComponent);
