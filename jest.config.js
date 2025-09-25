aaa0f9b7044636690ea001cc6a6f437fda34fd17
Jacek Dargiel <jacek.dargiel@10clouds.com>
2025-09-25 17:13:07 +0200

    Ignore e2e tests in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/e2e/'],
     transformIgnorePatterns: [
         '/node_modules/(?!flat)/',
     ],
