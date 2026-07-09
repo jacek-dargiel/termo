1b939e0d9ef64e19203a65ddcecd77b27c7cb60f
Jacek Dargiel <git@dargiel.net>
2026-07-09 15:46:25 +0200

    chore: remove debug logging from sentry-release.sh

--- bin/sentry-release.sh
+++ bin/sentry-release.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-export SENTRY_LOG_LEVEL=debug
-
 VERSION=$(sentry-cli releases propose-version)
-
-# Debug: verify auth and list projects
-sentry-cli projects list -o "$SENTRY_ORG" || true
 
 # Create a release
 sentry-cli releases new -p termo -o "$SENTRY_ORG" "$VERSION"
 
-# Associate commits with the release using local git history (works in CI without Sentry repo integration)
+# Associate commits with the release using local git history
 sentry-cli releases set-commits --local -p termo -o "$SENTRY_ORG" "$VERSION"
