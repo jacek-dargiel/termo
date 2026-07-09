6a030fdd9eb7be274a0230d49897d6558b4734f5
Jacek Dargiel <git@dargiel.net>
2026-07-09 14:40:27 +0200

    debug: add sentry-cli debug logging to diagnose project not found

--- bin/sentry-release.sh
+++ bin/sentry-release.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+export SENTRY_LOG_LEVEL=debug
+
 VERSION=$(sentry-cli releases propose-version)
+
+# Debug: verify auth and list projects
+sentry-cli projects list -o "$SENTRY_ORG" || true
 
 # Create a release
 sentry-cli releases new -p termo -o "$SENTRY_ORG" "$VERSION"
